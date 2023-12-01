28329128c55b4122263744a2ccdf897d298b0d4d
3raphat <treeraphat234@gmail.com>
2023-12-01 23:46:16 +0700

    Add redirect for /invite route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/invite',
+        destination:
+          'https://discord.com/api/oauth2/authorize?client_id=995348230449672212&permissions=274881334272&scope=bot%20applications.commands',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
